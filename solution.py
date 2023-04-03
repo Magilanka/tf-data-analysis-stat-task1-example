569c4f1a35e93896247b21d037f409b69d754237
Марина <Magilanka@users.noreply.github.com>
2023-04-03 21:11:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,12 @@ import numpy as np
 chat_id = 706100023 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
+    n = len(x)
+    t_value = t.ppf(0.975, n-1)
+    error = 49 + np.exp(1)
+    ci_lower = a - t_value * error / np.sqrt(n)
+    ci_upper = a + t_value * error / np.sqrt(n)
     # Измените код этой функции sss
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return x.mean() / 10 # Ваш ответ
