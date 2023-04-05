74bc0e276b979bc0fde2afe2dd451bcbdfbac4fc
Марина <Magilanka@users.noreply.github.com>
2023-04-05 11:04:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,11 +5,12 @@ import numpy as np
 chat_id = 706100023 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-#     n = len(x)
-#     t_value = t.ppf(0.975, n-1)
-#     error = -49 + np.exp(1)
-#     ci_lower = a - t_value * error / np.sqrt(n)
-#     ci_upper = a + t_value * error / np.sqrt(n)
+     n = len(x)
+     a = v / t
+    t_value = t.ppf(0.975, n-1)
+    error = -49 + np.exp(1)
+    ci_lower = a - t_value * error / np.sqrt(n)
+    ci_upper = a + t_value * error / np.sqrt(n)
     # Измените код этой функции sss
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
