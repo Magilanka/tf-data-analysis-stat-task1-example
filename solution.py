96fd82aae8d6f937110f8bb2c628dafbb8d1e19f
Марина <Magilanka@users.noreply.github.com>
2023-04-05 12:33:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,13 +5,23 @@ import numpy as np
 chat_id = 706100023 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-     n = len(x)
-     a = v / t
-    t_value = t.ppf(0.975, n-1)
-    error = -49 + np.exp(1)
-    ci_lower = a - t_value * error / np.sqrt(n)
-    ci_upper = a + t_value * error / np.sqrt(n)
+     from scipy.stats import norm
+     error_dist = lambda n: -49 + np.exp(1)
+     n=100
+     v_true = np.random.uniform(0, 50, n) # истинные значения скоростей
+     v_error = error_dist(n) # случайная ошибка измерения скорости
+     v_observed = v_true + v_error # наблюдаемые значения скоростей
+     v_mean = np.mean(v_observed) # Вычисляем среднюю скорость
+     a_hat = (v_mean) / 10 # Вычисляем оценку коэффициента ускорения
+#      n = len(x)
+#      a = v / t
+#     t_value = t.ppf(0.975, n-1)
+#     error = -49 + np.exp(1)
+#     ci_lower = a - t_value * error / np.sqrt(n)
+#     ci_upper = a + t_value * error / np.sqrt(n)
     # Измените код этой функции sss
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() / 10 # Ваш ответ
+# np.sqrt(n) - корень
+# var(x) - дисперсия
+    return a_hat # Ваш ответ
