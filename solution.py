8feb35afa05728757cd956f15d7c3a943fcf16c2
Марина <Magilanka@users.noreply.github.com>
2023-04-07 14:35:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,15 @@ import numpy as np
 chat_id = 706100023 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-     speeds = x
-     error_dist = lambda x: -49 + np.random.exponential(1)
-     measurements = speeds + np.array([error_dist(x) for x in range(len(speeds))])
-     variances = np.array([1/np.exp(1)**2, 1/np.exp(2)**2, 1/np.exp(3)**2])
-     weights = 1/variances
-     theta_hat = np.sum(weights * measurements) / np.sum(weights)
-     return theta_hat 
+    speeds = x
+    num = len(x)
+    error_dist = lambda x: -49 + np.random.exponential(1)
+    measurements =[]
+    for i in range(num):
+      measurements = speeds + np.array([error_dist(x) for x in range(num)])
+    variances = []
+    for i in range(num):
+      variances.append(1/np.exp(i+1)**2)
+    weights = 1/np.asarray(variances)
+    theta_hat = np.sum(weights * measurements) / np.sum(weights)
+    return theta_hat
